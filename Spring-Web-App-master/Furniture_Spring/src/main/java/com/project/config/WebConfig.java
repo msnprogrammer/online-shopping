fb5bda7ee726ace05b6b7dd2b94797f37675a57a
msn <srinathmanchala@gmail.com>
2019-05-14 10:33:37 +0530

    Add files via upload

---
+++ Spring-Web-App-master/Furniture_Spring/src/main/java/com/project/config/WebConfig.java
@@ -0,0 +1,74 @@
+package com.project.config;
+
+
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.project")
+public class WebConfig extends WebMvcConfigurerAdapter{
+	@Autowired
+	DataSource dataSource;
+	
+	@Bean
+	public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
+	
+	@Bean
+	public DataSource getDataSource() throws NamingException {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/muscle");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Telugu@1");
+		return dataSource;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		 registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/");
+         
+	
+	}
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Override
+	  public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addViewController("/").setViewName("index");
+	  }
+	
+	
+	
+	
+	
+	
+	
+	
+}
